3a63e97fae68a98f8bd8f3201f077345e4d16813
Lucas Amado <lucas.amado@incm.pt>
2022-12-20 19:47:07 +0000

    Create CycloidEpgApplicationTests.java

---
+++ src/test/java/pt/amado/cycloidepg/CycloidEpgApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amado.cycloidepg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycloidEpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
